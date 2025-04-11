e56f264e3ac094d6e268afd91cd342ae3e956880
炸蛋纽er <z1436910962@gmail.com>
2025-04-11 22:51:07 +0200

    win11-temp

--- LeetCode/src/main/java/tree2/DeleteBst.java
+++ LeetCode/src/main/java/tree2/DeleteBst.java
@@ -18,6 +18,28 @@ public class DeleteBst {
     }
 
     public TreeNode deleteNode(TreeNode root, int key) {
-        return null;
+        if (root == null) return null;
+
+        if (root.val == key) {
+            if (root.left == null && root.right == null) return null;
+            if (root.left == null) {
+                return root.right;
+            }
+            if (root.right == null) {
+                return root.left;
+            }
+            TreeNode leftNode = root.left;
+            TreeNode rightNode = root.right;
+            while (rightNode.left != null) {
+                rightNode = rightNode.left;
+            }
+            rightNode = leftNode;
+            return root.right;
+        }
+
+        root.left = deleteNode(root.left, key);
+        root.right = deleteNode(root.right, key);
+
+        return root;
     }
 }
